afdbb4cb55116409fa513ab8f84d4fa1331dbe0d
kamal <kmamunuri@nisum.com>
2016-04-12 10:18:42 -0700

    kamal New Swagger version to support ResponseEntity

--- swagger-sw/src/main/java/com/sw/springmvc/controller/HelloWorldController.java
+++ swagger-sw/src/main/java/com/sw/springmvc/controller/HelloWorldController.java
@@ -1,19 +1,25 @@
 package com.sw.springmvc.controller;
 
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
 
 @Controller
-@RequestMapping(value = "/hello", produces = MediaType.TEXT_PLAIN_VALUE)
 public class HelloWorldController {
 
-    @RequestMapping( produces = MediaType.TEXT_PLAIN_VALUE, method = RequestMethod.GET)
-    public @ResponseBody String sayHelloWorld(){
+	@ApiOperation(value = "This is Hello World Service")
+	@RequestMapping(value = "/hello/{input}", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public ResponseEntity<?> get(
+			@ApiParam(value = "input", required = true) @PathVariable(value = "input") String input) {
+		return new ResponseEntity<String>(input, HttpStatus.OK);
+	}
 
-        return "Hello World!";
-    }
 }
